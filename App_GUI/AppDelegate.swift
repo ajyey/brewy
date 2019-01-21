1675c751842f55cec2df1542ee5aa7ee7d449f04
Andrew Leonard <andrewleonard1693@gmail.com>
2019-01-21 03:40:49 -0500

    Initial Commit

---
+++ App_GUI/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  App_GUI
+//
+//  Created by AJ Leonard on 1/21/19.
+//  Copyright © 2019 AJ Leonard. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ App_GUI/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  App_GUI
+//
+//  Created by AJ Leonard on 1/21/19.
+//  Copyright © 2019 AJ Leonard. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
