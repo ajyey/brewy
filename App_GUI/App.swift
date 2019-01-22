3a284e12c2a8187d843a1a3a939b99ae156b7d36
Andrew Leonard <andrewleonard1693@gmail.com>
2019-01-22 01:12:26 -0500

    Add App object swift file

---
+++ App_GUI/App.swift
@@ -0,0 +1,37 @@
+//
+//  App.swift
+//  App_GUI
+//
+//  Created by AJ Leonard on 1/22/19.
+//  Copyright © 2019 AJ Leonard. All rights reserved.
+//
+
+import Foundation
+
+class App {
+    var github_raw: String
+    var name: String?
+    var url: String?
+    var version: String?
+    
+    //MARK: Initialization
+    init?(github_raw:String){
+        //test for github_raw empty
+        if(github_raw.isEmpty){
+            return nil
+        }
+        self.github_raw = github_raw
+
+    }
+    
+    func parseGithubRaw(){
+        //parse github raw file and set this app's fields
+        //Order for parsing
+        /*
+         1. Version
+         2. url
+         3. App name
+         
+         */
+    }
+}
